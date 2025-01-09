4588ec05c437a9efecc0e585d2b63d51d516a271
Prince Bhatia <princebhatia01@gmail.com>
2025-01-09 14:03:52 +1100

    updating playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,18 @@
-module.exports = {
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  name: "My Playwright Project",
   testDir: "tests",
   timeout: 30000,
   use: {
     browserName: "chromium",
     headless: true,
   },
-};
+  projects: [
+    {
+      name: "Playwright",
+      use: { browserName: "chromium" },
+    },
+  ],
+  reporter: [["list"], ["json", { outputFile: "test-results.json" }]],
+});
